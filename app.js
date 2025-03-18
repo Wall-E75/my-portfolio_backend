bf9c38bec0bcf07f65ea6c04af85bc91a9446edf
Wali <walisylla.esse@gmail.com>
2025-03-18 18:27:39 +0100

    mise en place de cors

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+require("./models/connection"); // Fichier de connection à la BDD très important
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -7,6 +10,25 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+const cors = require('cors');
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}
+
+app.use(cors(corsOptions)); // On ajoute cors à notre application
 
 app.use(logger('dev'));
 app.use(express.json());
