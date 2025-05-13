8262a8825a7159b02fadbce5c4bd411af30a6782
Wali <walisylla.esse@gmail.com>
2025-05-13 19:11:12 +0200

    cors

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const cors = require('cors');
 
 const allowedOrigins = process.env.ALLOWED_ORIGINS
     ? process.env.ALLOWED_ORIGINS.split(',')
-    : ["http://localhost:3001", "http://localhost:3000", "http://127.0.0.1:3000", "http://0.0.0.0:3001", "https://my-portfolio-backend-sage.vercel.app/message"];
+    : ["http://localhost:3001", "http://localhost:3000", "http://127.0.0.1:3000", "http://0.0.0.0:3001", "https://my-portfolio-backend-sage.vercel.app"];
 
 const corsOptions = {
     origin: function (origin, callback) {
