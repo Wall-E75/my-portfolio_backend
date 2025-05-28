20dac137e141c9a375bc5c9fffc4dc132e9020c1
Wali <walisylla.esse@gmail.com>
2025-05-28 19:10:33 +0200

    fin du back

--- services/emailService.js
+++ services/emailService.js
@@ -3,9 +3,15 @@ const rateLimit = require('express-rate-limit');
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true pour 465, false pour autres ports
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 })
 //Protection anti-spam
